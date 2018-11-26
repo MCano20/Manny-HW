4c13849c6b7465998d6922f46c6e7853787f41af
MCano20 <45370765+MCano20@users.noreply.github.com>
2018-11-26 13:55:21 -0600

    Add files via upload

---
+++ HW 11/something.js
@@ -0,0 +1,24 @@
+function elMM() {
+    let theh1 = document.getElementById('div1');
+    theh1.style.backgroundColor = 'black';
+    theh1.style.textAlign = 'right';
+}
+
+
+function elNene() {
+    let theDiv = document.getElementById('div2');
+    theDiv.style.backgroundColor = 'yellow';
+    theDiv.style.fontSize = '4rem';
+}
+
+function broChacho() {
+    let theDiv = document.getElementById('div3');
+    theDiv.style.textAlign = 'right';
+    theDiv.style.fontSize = '4rem';
+}
+
+const changeP = () => {
+    let paragrpah = document.getElementById('p96');
+    paragrpah.classList.add('p');
+    
+}
